d1996cd1601960c23fae139573dea4454b9a387a
Rails Udemy Course <jay_lin@mydearteacher.com>
2018-05-08 23:55:06 +0000

    add article and category association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ActiveRecord::Base
   belongs_to :user
+  has_many :article_categories
+  has_many :categories, through: :article_categories
   validates :user_id, presence: true
 end
